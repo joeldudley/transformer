df6361d47f9a6ad76d4be17d0465cc4d76e82931
joeldudley <joel.l.dudley@gmail.com>
2024-01-12 14:52:45 +0000

    When training, print avg loss over multiple steps.

--- Model.py
+++ Model.py
@@ -5,6 +5,8 @@ from DataSet import DataSet
 from Learner import Learner
 from Vectoriser import Vectoriser
 
+EVAL_FREQ = 200
+
 
 class Model:
     def __init__(self, text: str):
@@ -13,12 +15,18 @@ class Model:
         self.learner = Learner(self.vectoriser.vocab_size)
 
     def train(self, steps: int):
+        total_loss = 0
+
         for step in range(steps):
             xb, yb = self.data_set.get_train_batch()
             loss = self.learner(xb, yb)
             self._update_gradients(loss)
-            rounded_loss = "%.2f" % loss.item()
-            print(f"Step {step + 1}: {rounded_loss}")
+            total_loss += loss.item()
+
+            if (step + 1) % EVAL_FREQ == 0:
+                rounded_loss = "%.2f" % (total_loss / EVAL_FREQ)
+                total_loss = 0
+                print(f"Step {step + 1}: {rounded_loss}")
 
     def generate(self, token_qty: int):
         context = torch.zeros((1, 1), dtype=torch.long)
