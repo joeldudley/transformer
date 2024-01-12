04a258df9ea4466097b6b9806eb436d4a56a9366
joeldudley <joel.l.dudley@gmail.com>
2024-01-12 13:44:36 +0000

    Refactoring bigram model.

--- BigramLanguageModel.py
+++ BigramLanguageModel.py
@@ -1,29 +1,34 @@
 import torch
 from torch import nn, Tensor
-from torch.nn import functional
+from torch.nn import functional, Embedding
 
 
 class BigramLanguageModel(nn.Module):
     def __init__(self, vocab_size: int):
         super().__init__()
-        # We initialise our embedding randomly.
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+        self.token_embedding_table = self._get_random_embeddings(vocab_size)
 
     def forward(self, blocks: Tensor, targets: Tensor) -> Tensor:
-        per_char_logits = self.token_embedding_table(blocks)
-        return self._cross_entropy(per_char_logits, targets)
+        per_token_logits = self.token_embedding_table(blocks)
+        return self._cross_entropy(per_token_logits, targets)
 
-    def generate(self, initial_chars: Tensor, tokens_to_generate: int):
-        chars = initial_chars
+    def generate(self, initial_tokens: Tensor, tokens_to_generate: int) -> Tensor:
+        output_tokens = initial_tokens
 
         for _ in range(tokens_to_generate):
-            next_char_logits = self.token_embedding_table(chars)[:, -1, :]
-            # Convert the logits into a probability for each char.
-            next_char_probs = functional.softmax(next_char_logits, dim=1)
-            next_char = torch.multinomial(next_char_probs, num_samples=1)
-            chars = torch.cat((chars, next_char), dim=1)
+            next_token = self.generate_next_token(output_tokens)
+            output_tokens = torch.cat((output_tokens, next_token), dim=1)
 
-        return chars
+        return output_tokens
+
+    def generate_next_token(self, output_chars) -> Tensor:
+        next_token_logits = self.token_embedding_table(output_chars)[:, -1, :]
+        next_token_probs = functional.softmax(next_token_logits, dim=1)
+        return torch.multinomial(next_token_probs, num_samples=1)
+
+    @staticmethod
+    def _get_random_embeddings(vocab_size) -> Embedding:
+        return nn.Embedding(vocab_size, vocab_size)
 
     @staticmethod
     def _cross_entropy(per_char_logits: Tensor, targets: Tensor) -> Tensor:
